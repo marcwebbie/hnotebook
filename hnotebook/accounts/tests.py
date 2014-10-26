330865df99724391b58fab7dcb0ef3b3d75cd04b
marcwebbie <marcwebbie@gmail.com>
2014-10-26 16:13:55 -0300

    Refactored tests for profile

--- hnotebook/accounts/tests.py
+++ hnotebook/accounts/tests.py
@@ -3,12 +3,10 @@ from .models import Profile
 
 class ProfileTestCases(TestCase):
     def setUp(self):
-        pass
+        self.new_profile = Profile.objects.create(first_name="John", last_name="Doe")
 
     def test_new_profile_has_no_notebook(self):
-        profile = Profile.objects.create(first_name="John", last_name="Doe")
-        self.assertEqual(profile.notebooks.count(), 0)
+        self.assertEqual(self.new_profile.notebooks.count(), 0)
 
     def test_new_profile_has_zero_bookmarks(self):
-        profile = Profile.objects.create(first_name="John", last_name="Doe")
-        self.assertEqual(profile.bookmarks.count(), 0)
+        self.assertEqual(self.new_profile.bookmarks.count(), 0)
