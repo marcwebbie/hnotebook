1c484beb6830f6bec9d2ced8b0945478328a6a5c
marcwebbie <marcwebbie@gmail.com>
2014-10-26 18:22:24 -0300

    Added tests to inserting new notebooks into profiles

--- hnotebook/accounts/tests.py
+++ hnotebook/accounts/tests.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
 from .models import Profile
 from .factories import UserFactory
+from hnotebook.notebooks.factories import NotebookFactory
 
-class ProfileTestCases(TestCase):
+class ProfileModelTestCases(TestCase):
     def setUp(self):
         self.user = UserFactory()
         self.new_profile = Profile.objects.create(user=self.user)
@@ -18,3 +19,8 @@ class ProfileTestCases(TestCase):
 
     def test_profile_is_associated_with_user(self):
         self.assertIsInstance(self.new_profile.user, type(self.user))
+
+    def test_add_notebook_to_profile(self):
+        # import pdb; pdb.set_trace()
+        notebook = NotebookFactory()
+        self.new_profile.notebooks.add(notebook)
