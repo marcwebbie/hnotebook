f1eb86af796060f279d7eb0ac1fc8aa9bb333a31
marcwebbie <marcwebbie@gmail.com>
2014-10-26 17:58:57 -0300

    Added user factory

---
+++ hnotebook/accounts/factories.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
+
+import factory
+
+class UserFactory(factory.DjangoModelFactory):
+    FACTORY_FOR = User
+    FACTORY_DJANGO_GET_OR_CREATE = ('username',)
+
+    first_name = factory.LazyAttribute(lambda o: 'John')
+    last_name = factory.LazyAttribute(lambda o: 'Doe')
+    password = make_password('password')
+
+    @factory.Sequence
+    def username(n):
+        return 'user{}'.format(n)
+
+    email = factory.LazyAttribute(lambda o: '{0.username}@example.com'.format(o))
