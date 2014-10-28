c5cf6b134014320f46394cb118f0e5c9e17dfe5e
marcwebbie <marcwebbie@gmail.com>
2014-10-28 15:56:15 -0300

    Add Housing admin

--- hnotebook/notebooks/admin.py
+++ hnotebook/notebooks/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from .models import Notebook
+from .models import Notebook, Housing
 
 class NotebookAdmin(admin.ModelAdmin):
     model = Notebook
 
+class HousingAdmin(admin.ModelAdmin):
+    model = Housing
+
 admin.site.register(Notebook, NotebookAdmin)
+admin.site.register(Housing, HousingAdmin)
