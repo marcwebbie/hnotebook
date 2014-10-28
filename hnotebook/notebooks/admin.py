0fc338878e2888bee405af4bd6f1eef39323b6fb
marcwebbie <marcwebbie@gmail.com>
2014-10-28 15:58:27 -0300

    Add Review admin

--- hnotebook/notebooks/admin.py
+++ hnotebook/notebooks/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Notebook, Housing
+from .models import Notebook, Housing, Review
 
 class NotebookAdmin(admin.ModelAdmin):
     model = Notebook
@@ -8,5 +8,9 @@ class NotebookAdmin(admin.ModelAdmin):
 class HousingAdmin(admin.ModelAdmin):
     model = Housing
 
+class ReviewAdmin(admin.ModelAdmin):
+    model = Review
+
 admin.site.register(Notebook, NotebookAdmin)
 admin.site.register(Housing, HousingAdmin)
+admin.site.register(Review, ReviewAdmin)
