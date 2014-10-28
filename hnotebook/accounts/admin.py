83605d01594f950adc6bf24df305ab8bd6747df2
marcwebbie <marcwebbie@gmail.com>
2014-10-28 15:49:40 -0300

    Added admin for User and Profile

--- hnotebook/accounts/admin.py
+++ hnotebook/accounts/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group, User
 
-# Register your models here.
+from .models import Profile
+
+class ProfileInline(admin.StackedInline):
+    """Define an inline admin descriptor for Employee model
+    which acts a bit like a singleton"""
+    model = Profile
+    can_delete = False
+
+class UserAdmin(UserAdmin):
+    """Defines a new user admin"""
+    inlines = (ProfileInline, )
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+# Uregister group
+admin.site.unregister(Group)
