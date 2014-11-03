8157d76488d08875f9cec5105aa7547cefa02245
marcwebbie <marcwebbie@gmail.com>
2014-11-03 01:28:02 -0300

    Refactoring to use reverse for urls

--- hnotebook/tests.py
+++ hnotebook/tests.py
@@ -1,4 +1,4 @@
-from django.core.urlresolvers import resolve
+from django.core.urlresolvers import resolve, reverse
 from django.test import TestCase, Client
 
 from . import views
@@ -9,29 +9,29 @@ class HomePageTestCase(TestCase):
         self.client = Client()
 
     def test_root_url_returns_home_page(self):
-        response = self.client.get('/')
+        response = self.client.get(reverse('homepage'))
         self.assertEqual(response.status_code, 200)
 
     def test_homepage_has_hnotebook_in_title(self):
-        response = self.client.get('/')
+        response = self.client.get(reverse('homepage'))
         self.assertIn("homepage", str(response.content).lower())
 
     def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
+        found = resolve(reverse('homepage'))
         self.assertEqual(found.func, views.homepage)
 
     def test_homepage_has_login_string(self):
-        response = self.client.get('/')
+        response = self.client.get(reverse('homepage'))
         self.assertIn("login", str(response.content).lower())
 
     def test_homepage_has_login_string(self):
-        response = self.client.get('/')
+        response = self.client.get(reverse('homepage'))
         self.assertIn("sign up", str(response.content).lower())
 
     def test_login_url_renders_login_template(self):
-        found = resolve('/login/')
+        found = resolve(reverse('login'))
         self.assertEqual(found.kwargs['template_name'], 'login.html')
 
     def test_homepage_has_login_string(self):
-        response = self.client.get('/signup/')
+        response = self.client.get(reverse('signup'))
         self.assertIn("sign up", str(response.content).lower())
--- hnotebook/urls.py
+++ hnotebook/urls.py
@@ -4,14 +4,9 @@ from django.contrib import admin
 from . import views as main_views
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'hnotebook.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', main_views.homepage),
+    url(r'^$', main_views.homepage, name='homepage'),
     url(r'^signup/', main_views.signup,name='signup'),
     url(r'^login/', 'django.contrib.auth.views.login',
         {'template_name': 'login.html'}, name='login'),
-
 )
